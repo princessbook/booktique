f5b3d44131d5ab401cc9f8547a933f95f24a4189
HwangHyeonmi <49122297+HwangHyeonmi@users.noreply.github.com>
2024-04-04 22:06:47 +0900

    #57 [feat]: 방장 나갔을 시 남아있는 멤버 중 한명이 방장 되게 처리

--- src/app/bookclubs/ClubAdminProfile.tsx
+++ src/app/bookclubs/ClubAdminProfile.tsx
@@ -22,6 +22,22 @@ const ClubAdminProfile = ({ clubId }: { clubId: string }) => {
 
         if (!adminMembers || adminMembers.length === 0) {
           console.log('No admin members found for the club.');
+          const { data: members, error: membersError } = await supabase
+            .from('members')
+            .select('*')
+            .eq('club_id', clubId);
+
+          if (members && members.length > 0) {
+            console.log('이거뜸?', members[0].user_id);
+            const { data: member, error } = await supabase
+              .from('members')
+              .update({ role: 'admin' })
+              .eq('user_id', members[0].user_id as string);
+
+            console.log('멤버롤이 어드민으로 업그레이드 됨');
+          }
+
+          console.log('남은멤버들', members);
           return [];
         }
 
@@ -44,14 +60,13 @@ const ClubAdminProfile = ({ clubId }: { clubId: string }) => {
     fetchAdminUserProfiles(clubId);
   }, [clubId]);
 
-  if (!adminProfile) {
-    return <></>;
-  }
   return (
     <div className='flex justify-between items-center mr-2'>
       <span className='bg-gray-300 w-5 h-5 rounded-full flex items-center justify-center '>
         <Image
-          src={adminProfile?.photo_URL as string}
+          src={
+            adminProfile ? (adminProfile.photo_URL as string) : '/booktique.png'
+          }
           width={100}
           height={100}
           alt='방장프로필'

01114ba1a6ae524f642ca2f2e04e9d56105d6856
HwangHyeonmi <49122297+HwangHyeonmi@users.noreply.github.com>
2024-04-05 17:24:23 +0900

    #68 [fix]:createClient 함수 서버,클라이언트당 인스턴스 한번만 생성

--- src/utils/supabase/client.ts
+++ src/utils/supabase/client.ts
@@ -8,3 +8,5 @@ export const createClient = (): SupabaseClient<Database> => {
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   );
 };
+
+export const supabaseCSR = createClient();
--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -32,3 +32,5 @@ export function createClient(): SupabaseClient<Database> {
     }
   );
 }
+
+export const supabaseSSR = createClient();

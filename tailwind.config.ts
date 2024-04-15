b58a824d952a3045aa74c094d1a753e21aca6ac7
HwangHyeonmi <49122297+HwangHyeonmi@users.noreply.github.com>
2024-04-15 19:52:12 +0900

    [style] : 태블릿,pc에서 모바일 크기로 보이게 조정, bottom navigation 스타일 수정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,7 +13,10 @@ const config: Config = {
   ],
   theme: {
     screens: {
-      xs: '375px'
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px'
     },
     extend: {
       screens: {

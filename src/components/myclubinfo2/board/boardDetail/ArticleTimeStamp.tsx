250204ce81dd3aeb31ee5b3eb165d72bc24c11b2
HuaHuaChiChi <ksksks102@naver.com>
2024-04-16 16:54:50 +0900

    #188[mod]: 충돌 수정

--- src/components/myclubinfo2/board/boardDetail/ArticleTimeStamp.tsx
+++ src/components/myclubinfo2/board/boardDetail/ArticleTimeStamp.tsx
@@ -2,8 +2,8 @@
 
 import useRelativeTime from '@/hooks/Board/useRelativeTime';
 
-const ArticleTimeStamp = ({ created_at }: { created_at: string }) => {
-  const res = useRelativeTime(created_at);
+const ArticleTimeStamp = ({ created_at }: { created_at: string | null }) => {
+  const res = useRelativeTime(created_at as string);
 
   return <p className={'text-xs text-gray-400'}>{res}</p>;
 };

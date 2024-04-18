5b34bf4ae46f62220abb823b69e3fc9dfbaf151b
Kim CG <cjfrbs1256@gmail.com>
2024-04-18 23:36:44 +0900

    배포주소 소셜로그인테스트 원상복구3

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,13 +3,7 @@ import { authToken } from '@/utils/middlewares/authToken';
 import { protectRoute } from '@/utils/middlewares/protectRoute';
 
 //middleware체인: 여러단계의 요청처리를 분할해서 chain으로 관리를합니다
-// export default chain([authToken, protectRoute]);
-import { type NextRequest } from 'next/server';
-import { updateSession } from './utils/middlewares/middleware';
-
-export async function middleware(request: NextRequest) {
-  return await updateSession(request);
-}
+export default chain([authToken, protectRoute]);
 
 export const config = {
   matcher: [

5f0ee9e260207c7160f87f44a5afb8670994501a
Kim CG <cjfrbs1256@gmail.com>
2024-04-19 16:42:12 +0900

    #291 [pr]:pr전 커밋

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import QueryProvider from './provider';
-export const dynamic = 'force-dynamic';
 
 const inter = Inter({ subsets: ['latin'] });
 

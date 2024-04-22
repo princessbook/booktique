3380b5fa8e6fd1414628c69017b58d3c4882ad5f
yeriniii <yerini000303@naver.com>
2024-04-22 19:55:51 +0900

    sentencePage getOrCreateUserProfile 삭제

--- src/app/(navigationBar)/my-clubs/[clubId]/sentences/page.tsx
+++ src/app/(navigationBar)/my-clubs/[clubId]/sentences/page.tsx
@@ -2,6 +2,7 @@
 
 import { getOrCreateUserProfile } from '@/app/auth/authAPI';
 import SentenceStorage from '@/components/my-clubs/sentences/SentenceStorage';
+import useMyClubInfo from '@/hooks/info/useMyClubInfo';
 import { Tables } from '@/lib/types/supabase';
 import {
   getUserId,
@@ -21,28 +22,28 @@ const SentencePage = (props: Props) => {
   }>();
   const [clubs, setClubs] = useState<Clubs[]>([]);
   const [userId, setUserId] = useState<string | null>(null);
-
+  // const {clubs}=useMyClubInfo();
   useEffect(() => {
     const fetchData = async () => {
       try {
         const fetchedUserId = await getUserId();
         setUserId(fetchedUserId);
         if (fetchedUserId) {
-          const fetchedUserProfile = await getOrCreateUserProfile();
-          if (fetchedUserProfile) {
-            const fetchedClubIds = await getUserClubIds(fetchedUserId);
-            const fetchClubInfo = await getClubInfo(
-              fetchedClubIds.filter(
-                (id) => !clubs.find((club) => club.id === id)?.archive
-              )
-            );
-            setClubs(fetchClubInfo);
+          // const fetchedUserProfile = await getOrCreateUserProfile();
+          // if (fetchedUserProfile) {
+          const fetchedClubIds = await getUserClubIds(fetchedUserId);
+          const fetchClubInfo = await getClubInfo(
+            fetchedClubIds.filter(
+              (id) => !clubs.find((club) => club.id === id)?.archive
+            )
+          );
+          setClubs(fetchClubInfo);
 
-            // TODO: 이거 나중에 처리하기
-            // if (!selectedClubId && fetchedClubIds.length > 0) {
-            //   setSelectedClubId(fetchClubInfo[0].id); // 첫 번째 북클럽을 선택
-            // }
-          }
+          // TODO: 이거 나중에 처리하기
+          // if (!selectedClubId && fetchedClubIds.length > 0) {
+          //   setSelectedClubId(fetchClubInfo[0].id); // 첫 번째 북클럽을 선택
+          // }
+          // }
         }
       } catch (error) {
         console.error('Error fetching data:', error);

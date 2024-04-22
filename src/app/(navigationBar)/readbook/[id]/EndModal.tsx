345b772949e89be1f4e2e089cdca59470ce85e46
rlaeodus <gwg03185@gmail.com>
2024-04-22 23:42:35 +0900

    #366 [desgin] - 디자인 변경에 따른 수정

--- src/app/(navigationBar)/readbook/[id]/EndModal.tsx
+++ src/app/(navigationBar)/readbook/[id]/EndModal.tsx
@@ -1,7 +1,9 @@
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import React from 'react';
-import readbook_yet_read from '../../../../../public/readbook_yet_read.png';
+// import readbook_yet_read from '../../../../../public/readbook_yet_read.png';
+import readbook_completed_read from '../../../../../public/readbook_completed_read.png';
+
 interface ModalProps {
   onClose: () => void;
   id: string;
@@ -21,15 +23,17 @@ const EndModal = ({ onClose, id }: ModalProps) => {
     <div className='fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-20'>
       <div className='bg-white w-[327px] h-[412px] rounded-[20px] flex flex-col items-center'>
         <Image
-          src={readbook_yet_read}
+          // src={readbook_yet_read}
+          src={readbook_completed_read}
           alt='end'
           width={144}
           height={144}
           className='mt-[38px] mb-[16px] w-[144px] h-[144px]'
         />
         <h1 className=' mb-[32px] font-medium text-[16px] leading-[22px] text-center text-[#3F3E4E]'>
-          아직 책 읽기로한 시간을 채우지 못했어요. <br />
-          정말 책 읽기를 종료하시나요?
+          {/* 아직 책 읽기로한 시간을 채우지 못했어요. <br />
+          정말 책 읽기를 종료하시나요? */}
+          책 읽기를 종료하시나요?
         </h1>
         <div className='flex flex-col gap-[12px] w-[279px]'>
           <button

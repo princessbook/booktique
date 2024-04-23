bb03044e29ef21cb5e3bd7fe6a11606e58fa2541
yeriniii <yerini000303@naver.com>
2024-04-23 16:09:57 +0900

    [design]:border 크기랑 색 수정

--- src/app/(navigationBar)/mypage/mybookclubs/page.tsx
+++ src/app/(navigationBar)/mypage/mybookclubs/page.tsx
@@ -12,7 +12,7 @@ const MyBookClubsPage = async () => {
   }
   return (
     <div>
-      <div className='w-full flex-row sticky top-0 left-0 right-0 z-10 flex items-center mb-6 border-b-2 h-[58px] p-4 bg-white'>
+      <div className='w-full flex-row sticky top-0 left-0 right-0 z-10 flex items-center mb-6 border-b-[1px] border-[#DBE3EB] h-[58px] p-4 bg-white'>
         <BackBtn />
         <p className='items-center flex-grow text-center font-bold text-[17px] mr-4 text-fontTitle'>
           내 북클럽

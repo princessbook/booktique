708b322fc11baa044442eda0f81d88d2ed75635d
yeriniii <yerini000303@naver.com>
2024-04-23 20:12:44 +0900

    [refactor]:데이터가져오는부분 join으로 변경하기

--- src/components/mypage/sentences/MySentencesStore.tsx
+++ src/components/mypage/sentences/MySentencesStore.tsx
@@ -2,17 +2,14 @@ import React from 'react';
 import useUserSentences from '@/hooks/mypage/useUserSentences';
 import SentenceItem from './SentenceItem';
 const MySentencesStore = async ({ userId }: { userId: string }) => {
-  const { userSentences, clubsData } = await useUserSentences(userId);
+  const userSentences = await useUserSentences(userId);
 
   const firstThreeSentences = userSentences.slice(0, 3);
   return (
     <div className='mt-6'>
       <ul>
         {firstThreeSentences.map((sentence) => {
-          const club = clubsData?.find((club) => club.id === sentence.club_id);
-          return (
-            <SentenceItem key={sentence.id} sentence={sentence} club={club} />
-          );
+          return <SentenceItem key={sentence.id} sentence={sentence} />;
         })}
       </ul>
     </div>

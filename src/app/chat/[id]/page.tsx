d5170ddfade2ad6c9f4df3d2a110e1f45b5e77fa
Kim CG <cjfrbs1256@gmail.com>
2024-04-25 05:23:25 +0900

    commit 테스트

--- src/app/chat/[id]/page.tsx
+++ src/app/chat/[id]/page.tsx
@@ -3,6 +3,7 @@ import ChatInput from './ChatInput';
 import ChatMessages from '@/components/realtime/ChatMessages';
 import React, { Suspense } from 'react';
 
+//
 const ChatPage = async () => {
   // return (
   //   <Suspense fallback={'loading...'}>

c20f052f2cbba30a4e581dd64f9647ef03e556cc
HuaHuaChiChi <ksksks102@naver.com>
2024-04-25 20:47:32 +0900

    [feat]: PWA 테스트

--- next.config.js
+++ next.config.js
@@ -8,3 +8,23 @@ module.exports = {
     ] // 외부 호스트를 여기에 추가합니다.
   }
 };
+
+const {
+  PHASE_DEVELOPMENT_SERVER,
+  PHASE_PRODUCTION_BUILD
+} = require('next/constants');
+
+/** @type {import("next").NextConfig} */
+const nextConfig = {
+  reactStrictMode: true
+};
+
+module.exports = (phase) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
+    const withPWA = require('@ducanh2912/next-pwa').default({
+      dest: 'public'
+    });
+    return withPWA(nextConfig);
+  }
+  return nextConfig;
+};

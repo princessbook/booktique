c0763204534be51b487cea4fa83ae96d00e172d9
Kim CG <cjfrbs1256@gmail.com>
2024-04-29 03:37:56 +0900

    #492 [feat]:서스팬스,온보딩추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,24 @@
 import { createClient } from '@/utils/supabase/server';
 import { redirect } from 'next/navigation';
-import React from 'react';
 export const dynamic = 'force-dynamic';
 import { getOrCreateUserProfile } from './auth/authAPI';
-const supabase = createClient();
+import OnboardingPage from '@/components/register/OnboardingPage';
 
+const supabase = createClient();
 const MainPage = async () => {
   const {
     data: { user }
   } = await supabase.auth.getUser();
   if (!user?.id) {
-    redirect('/login');
+    //비로그인시 로그인페이지
+    return <OnboardingPage />;
+    // redirect('/login');
   } else {
+    //로그인시 my-clubs
     await getOrCreateUserProfile();
     redirect('/my-clubs');
   }
-  return <div>page</div>;
+  return <OnboardingPage />;
 };
 
 export default MainPage;

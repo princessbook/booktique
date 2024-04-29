d3d7f5f4e99b18084ad3df1a4bf1b0c96d39caf9
rlaeodus <gwg03185@gmail.com>
2024-04-29 22:04:14 +0900

    #408 [mod] - 콘솔제거, 로딩추가

--- src/components/realtime/ChatMessages.tsx
+++ src/components/realtime/ChatMessages.tsx
@@ -15,7 +15,7 @@ const ChatMessages = async ({ id }: { id: string }) => {
   const getUser = await supabase.auth.getUser();
   const userId = getUser.data.user?.id;
 
-  console.log('데이터', getUser.data.user);
+  // console.log('데이터', getUser.data.user);
 
   return (
     <Suspense fallback={'loading...'}>

d155c6e00b1db1fd44881c42349b6958af2e5113
yeriniii <yerini000303@naver.com>
2024-05-01 17:20:55 +0900

    북클럽,문장 없을때..

--- src/components/mypage/clubs/AllMyBookClubs.tsx
+++ src/components/mypage/clubs/AllMyBookClubs.tsx
@@ -2,10 +2,17 @@ import React from 'react';
 import useUserClubs from '@/hooks/mypage/useUserClubs';
 import Link from 'next/link';
 import BookClubItem from './BookClubItem';
+import NoContentMessage from '@/components/common/NoContentMessage';
 const AllMyBookClubs = async ({ userId }: { userId: string }) => {
   const clubData = await useUserClubs(userId);
-  if (!clubData) {
-    return null;
+  if (clubData?.length === 0) {
+    return (
+      <div className='mt-[50px]'>
+        <NoContentMessage width={125} imgUrl='/no_book.png'>
+          가입한 북클럽이 없습니다.
+        </NoContentMessage>
+      </div>
+    );
   }
   return (
     <div>
--- src/components/mypage/sentences/AllMySentences.tsx
+++ src/components/mypage/sentences/AllMySentences.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
 import useUserSentences from '@/hooks/mypage/useUserSentences';
 import SentenceItem from './SentenceItem';
+import Image from 'next/image';
+import NoContentMessage from '@/components/common/NoContentMessage';
+
 const AllMySentences = async ({ userId }: { userId: string }) => {
   const userSentences = await useUserSentences(userId);
+  if (userSentences.length === 0) {
+    return (
+      <div>
+        <NoContentMessage imgUrl='/no_sentence.png' width={191}>
+          {' '}
+          책을 읽고 좋았던 문장을 저장해 <br />
+          클럽원들과 함께 나눠 보세요.
+        </NoContentMessage>
+      </div>
+    );
+  }
   return (
     <div>
       <ul>

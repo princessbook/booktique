74d82133f3e10bd36976a5c9eebe44a5edbcb285
Kim CG <cjfrbs1256@gmail.com>
2024-07-08 22:16:30 +0900

    테스트계정생성

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,13 +7,6 @@ export default chain([authToken, protectRoute]);
 
 export const config = {
   matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     * Feel free to modify this pattern to include more paths.
-     */
     '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)'
   ]
 };
